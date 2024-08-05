18ce9fcbd851f4104b55d9daf84138420e7db69e
Joseph Dey <josephdey@uchicago.edu>
2024-08-05 07:57:24 -0700

    set up schema for adding proposals to database

---
+++ src/models/Proposal.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const ProposalSchema = new mongoose.Schema({
+  offeredItems: [
+    {
+      item: String,
+      quantity: Number
+    }
+  ],
+  desiredItems: [
+    {
+      item: String,
+      quantity: Number
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Proposal', ProposalSchema);
